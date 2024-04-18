fbd4242839e659454f8b639f2f5a0be7e010bfc8
Chris <chirschbrich@smu.edu>
2024-04-18 15:01:39 -0500

    added notes + format

--- prog3.cpp
+++ prog3.cpp
@@ -6,8 +6,19 @@
 // If you do that. make sure you change that for the call from Decision1()
 pair<bool, Stuff3> DecisionRecur1(vector<Stuff3>& vec)
 {
-   Stuff3 s;
-   pair<bool, Stuff3> p = make_pair(false, s);
+   //need base cases for odd and even vector sizes
+
+   /*
+   divide vector into smaller vectors by passing in n/2 recursively
+   find what is true and compare it with the layer up
+      if nothing is true then dont
+   keep going up checking to see if the number is in the vector more than half the time
+   
+   
+   */
+   
+   Stuff3 s; //only creates a vector for compilation, will be deleted later
+   pair<bool, Stuff3> p = make_pair(false, s); //pair that will be returned, replace perameters with answer
    return p;
 }
 
@@ -22,8 +33,11 @@ pair<bool, Stuff3> Decision1(vector<Stuff3>& vec)
 // If you do that. make sure you change that for the call from Decision2()
 pair<bool, Stuff3> DecisionRecur2(vector<Stuff3>& vec)
 {
-   Stuff3 s;
-   pair<bool, Stuff3> p = make_pair(true, s);
+   //need base cases for odd and even vector sizes
+   
+   
+   Stuff3 s; //only creates a vector for compilation, will be deleted later
+   pair<bool, Stuff3> p = make_pair(true, s); //pair that will be returned, replace perameters with answer
    return p;
 }
 
